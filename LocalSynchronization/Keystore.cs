9eec8d16ec6836c328a26f35de6ab520055f1279
Lukas Panni <lukas.panni@outlook.de>
2021-12-26 14:15:01 +0100

    Add client certificate

---
+++ LocalSynchronization/Keystore.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Security;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LocalSynchronization;
+
+internal class Keystore
+{
+
+    public static X509Certificate2 GenerateSelfSignedCertificate(string commonName)
+    {
+        var ecdsa = ECDsa.Create(ECCurve.CreateFromValue("1.2.840.10045.3.1.7"));
+        var name = new X500DistinguishedName($"C=DE,CN={commonName}");
+        var request = new CertificateRequest(name, ecdsa, HashAlgorithmName.SHA256);
+        var cert = request.CreateSelfSigned(DateTimeOffset.UtcNow.AddSeconds(-5), DateTimeOffset.UtcNow.AddYears(1));
+        return new X509Certificate2(cert.Export(X509ContentType.Pkcs12));
+    }
+
+    public static bool ValidateServerCertificate(
+       object sender,
+       X509Certificate certificate,
+       X509Chain chain,
+       SslPolicyErrors sslPolicyErrors)
+    {
+        if (sslPolicyErrors == SslPolicyErrors.None)
+            return true;
+
+        Console.WriteLine("Certificate error: {0}", sslPolicyErrors);
+        if (chain.ChainStatus[0].Status == X509ChainStatusFlags.UntrustedRoot)
+        {
+            return true; // allow self signed certificate
+        }
+
+        return false;
+    }
+
+    public static bool ValidateClientCertificate(
+      object sender,
+      X509Certificate certificate,
+      X509Chain chain,
+      SslPolicyErrors sslPolicyErrors)
+    {
+        //TODO: accept without certificate in pairing mode, otherwise verify certificate 
+        return true;
+    }
+}
+
--- LocalSynchronization/TransportLayer/TlsTcpClientAdapter.cs
+++ LocalSynchronization/TransportLayer/TlsTcpClientAdapter.cs
@@ -1,7 +1,6 @@
 ﻿using System.Net;
 using System.Net.Security;
 using System.Net.Sockets;
-using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
 namespace LocalSynchronization;
@@ -13,7 +12,9 @@ internal enum TlsMode
 internal class TlsTcpClientAdapter : TcpClientAdapter
 {
     private SslStream? secureStream;
+    //TODO: maybe split client and server
     private X509Certificate2? serverCertificate;
+    private X509Certificate2Collection clientCertificates = new X509Certificate2Collection();
     private string targetHost;
 
     protected override Stream? CommunicationStream => secureStream;
@@ -29,11 +30,12 @@ internal class TlsTcpClientAdapter : TcpClientAdapter
         }
     }
 
-    public TlsTcpClientAdapter(TcpClient client, string host = "testserver") : base(client)
+    public TlsTcpClientAdapter(TcpClient client, string host = "testserver", X509Certificate? certificate = null) : base(client)
     {
         // client: gets initialized with not connected socket and a target host
         // tls init can only happen after connect
         targetHost = host;
+        if(certificate != null) clientCertificates.Add(certificate);
     }
 
     public async override Task ConnectAsync(IPEndPoint remoteEP)
@@ -52,49 +54,13 @@ internal class TlsTcpClientAdapter : TcpClientAdapter
     {
         if (mode == TlsMode.Server && serverCertificate != null)
         {
-            secureStream = new SslStream(Client.GetStream(), false, new RemoteCertificateValidationCallback(ValidateClientCertificate), null);
+            secureStream = new SslStream(Client.GetStream(), false, new RemoteCertificateValidationCallback(Keystore.ValidateClientCertificate), null);
             await secureStream.AuthenticateAsServerAsync(serverCertificate, clientCertificateRequired: true, checkCertificateRevocation: true);
         }
         if (mode == TlsMode.Client)
         {
-            secureStream = new SslStream(Client.GetStream(), false, new RemoteCertificateValidationCallback(ValidateServerCertificate), null);
-            await secureStream.AuthenticateAsClientAsync(targetHost);
+            secureStream = new SslStream(Client.GetStream(), false, new RemoteCertificateValidationCallback(Keystore.ValidateServerCertificate), null);
+            await secureStream.AuthenticateAsClientAsync(targetHost, clientCertificates, true);
         }
-    }
-
-    private bool ValidateServerCertificate(
-      object sender,
-      X509Certificate certificate,
-      X509Chain chain,
-      SslPolicyErrors sslPolicyErrors)
-    {
-        if (sslPolicyErrors == SslPolicyErrors.None)
-            return true;
-
-        Console.WriteLine("Certificate error: {0}", sslPolicyErrors);
-        if (chain.ChainStatus[0].Status == X509ChainStatusFlags.UntrustedRoot)
-        {
-            return true; // allow self signed certificate
-        }
-
-        return false;
-    }
-
-    private bool ValidateClientCertificate(
-      object sender,
-      X509Certificate certificate,
-      X509Chain chain,
-      SslPolicyErrors sslPolicyErrors)
-    { 
-        //TODO: accept without certificate in pairing mode, otherwise verify certificate 
-        return true;
-    }
-
-    public static X509Certificate2 GenerateSelfSignedCertificate()
-    {
-        var ecdsa = ECDsa.Create();
-        var request = new CertificateRequest("cn=testserver", ecdsa, HashAlgorithmName.SHA256);
-        var cert = request.CreateSelfSigned(DateTimeOffset.UtcNow.AddSeconds(-5), DateTimeOffset.UtcNow.AddYears(1));
-        return new X509Certificate2(cert.Export(X509ContentType.Pkcs12));
     }
 }
