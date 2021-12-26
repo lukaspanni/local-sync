2fc39dfb931ecb71640869a18d0baf7fe0f4e864
Lukas Panni <lukas.panni@outlook.de>
2021-12-26 10:33:03 +0100

    Create TclTcpTransportLayer

---
+++ LocalSynchronization/TlsTcpTransportLayer.cs
@@ -0,0 +1,28 @@
+﻿
+namespace LocalSynchronization;
+
+public class TlsTcpTransportLayer : ITransportLayer
+{
+    private CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+    public async Task<TransportLayerMessage> ReceiveMessage()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task SendMessage(TransportLayerMessage message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CancelRunningOperations()
+    {
+        tokenSource.Cancel();   
+    }
+
+    public void Dispose()
+    {
+
+    }
+}
+
